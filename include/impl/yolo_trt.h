d81b5f4b9cf253d2a5b476fe47b58064cbb1a336
booljin <booljin@163.com>
2025-07-01 14:57:02 +0800

    初步完成带任务流的模型调度器
    Signed-off-by: booljin <booljin@163.com>

---
+++ include/impl/yolo_trt.h
@@ -0,0 +1,23 @@
+﻿#ifndef __YOLO_TRT_H__
+#define __YOLO_TRT_H__
+#include "yolo_defines.h"
+#include "../src/trt_impl/model_trt.h"
+#include "../src/trt_impl/workspace_trt.h"
+#include "../src/trt_impl/taskflow_trt.h"
+#include "yolo.h"
+
+namespace YOLO{
+	struct TRT {};
+
+    template<>
+    struct BackendTraits<YOLO::TRT> {
+        using Model = YOLO::MODEL::TRT::Model;
+        using WorkSpace = YOLO::WORKSPACE::TRT::WorkSpace;
+        using TaskFlow = YOLO::TASK::TRT::TaskFlow;
+    };
+
+}
+
+
+
+#endif // __YOLO_TRT_H__
---
+++ src/trt_impl/yolo_trt_utils.cpp
@@ -0,0 +1,23 @@
+﻿#include "yolo_trt_utils.h"
+#include <opencv2/opencv.hpp>
+
+namespace YOLO {
+namespace UTILS{
+namespace TRT{
+void dump_preprocess_img(CudaMemory<float>& img, const std::string& path){
+    img.memcpy_to_cpu_sync();
+    cv::Mat dst(640, 640, CV_8UC3);
+    for(int h = 0; h < 640; h++){
+        for(int w = 0; w < 640; w++){
+            dst.at<cv::Vec3b>(h, w) = cv::Vec3b(
+                img.cpu()[640 * 640 * 2 + h * 640 + w] * 255,
+                img.cpu()[640 * 640 + h * 640 + w] * 255,
+                img.cpu()[h * 640 + w] * 255
+            );
+
+        }
+    }
+    cv::imwrite(path, dst);
+}
+
+}}}
