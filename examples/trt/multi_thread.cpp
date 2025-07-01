744c0cffc623f6a8c21816901b20b7a610468493
booljin <booljin@163.com>
2025-07-01 15:21:24 +0800

    添加一些例子

---
+++ examples/trt/multi_thread.cpp
@@ -0,0 +1,44 @@
+﻿
+#include <opencv2/opencv.hpp>
+
+#include "yolo_trt.h"
+#include <thread>
+
+int main(int argc, char const* argv[])
+{
+	//onnx_runtime_test();
+	try {
+		cv::Mat img = cv::imread("D:/work/yolo1/seg2seg/ori.bmp");
+
+		YOLO::YoloScheduler<YOLO::TRT> scheduler;
+		scheduler.load_model("D:/work/yolo1/seg2seg/cdw.onnx", "cdw");
+		auto m2 = scheduler.load_model("D:/work/yolo1/seg2seg/jdw.onnx", "jdw", "cdw");
+		
+		m2->set_confidence_threshold(0.4);
+		m2->set_nms_threshold(0.3);
+
+		scheduler.binding_taskflow();
+
+
+		auto start = std::chrono::system_clock::now();
+		std::vector<std::thread> threads;
+		for (int i = 0; i < 10; i++) {
+			threads.emplace_back(std::thread([&scheduler, &img]() {
+				auto workspace = scheduler.get_new_workspace();
+				scheduler.execute(img, workspace);
+			}));
+		}
+		for (auto& t : threads) {
+			t.join();
+		}
+		auto end = std::chrono::system_clock::now();
+		std::cout << "total cost" << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << std::endl;
+
+
+	}
+	catch (YOLO::YoloException& e) {
+		std::cout << e.what() << std::endl;
+	}
+	return 0;
+}
+
---
+++ examples/trt/taskflow.cpp
@@ -0,0 +1,45 @@
+﻿
+#include <opencv2/opencv.hpp>
+
+#include "yolo_trt.h"
+
+int main(int argc, char const* argv[])
+{
+	//onnx_runtime_test();
+	try {
+		cv::Mat img = cv::imread("D:/work/yolo1/seg2seg/ori.bmp");
+
+		YOLO::YoloScheduler<YOLO::TRT> scheduler;
+		scheduler.load_model("D:/work/yolo1/seg2seg/cdw.onnx", "cdw");
+		auto m2 = scheduler.load_model("D:/work/yolo1/seg2seg/jdw.onnx", "jdw", "cdw");
+		
+		m2->set_confidence_threshold(0.4);
+		m2->set_nms_threshold(0.3);
+
+		auto workspace = scheduler.default_workspace();
+		scheduler.binding_taskflow();
+
+		std::vector<YOLO::TASK::TaskResult> results;
+
+		auto start = std::chrono::system_clock::now();
+		for(int i = 0; i < 10; i++)
+			results = scheduler.execute(img, workspace);
+		auto end = std::chrono::system_clock::now();
+		std::cout << "avg cost" << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() / 10 << std::endl;
+
+		auto segment_ret = std::get_if<YOLO::TASK::SegmentResult>(&results.back());
+
+		if (segment_ret && segment_ret->masks.size() > 0&& segment_ret->bboxes.size() > 0) {
+			cv::imwrite("D:/work/yolo1/seg2seg/output.jpg", draw_mask(*segment_ret, img, true));
+		}
+		else {
+			std::cout << "found nothing" << std::endl;
+		}
+
+	}
+	catch (YOLO::YoloException& e) {
+		std::cout << e.what() << std::endl;
+	}
+	return 0;
+}
+
